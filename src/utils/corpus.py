0544de00ff60d1e818c465090a17817f066449ed
Ndil-Adjim Sou Ngadoy <nasoungadoy@gmail.com>
2019-05-12 21:44:22 +0100

    add corpus and model wrapping utilities

--- src/utils/corpus.py
+++ src/utils/corpus.py
@@ -1,3 +1,113 @@
+from __future__ import annotations
+from typing import Sequence, Tuple, Optional, Union, List, Iterator
+import zipfile
+from pathlib import Path
+import os
+
+import pandas as pd
 import spacy
+from spacy.tokens.doc import Doc
+from spacy.language import Language
+import numpy as np
 import nltk
 
+class Corpus:
+    '''
+    Container for a corpus of Spacy Docs
+    '''
+    def __init__(self, nlp: Language, *, indexed_documents: Optional[Sequence[Tuple[Doc, int]]] = None,
+                 documents_series: Optional[pd.Series] = None):
+        self.nlp = nlp
+        self._documents: pd.Series
+
+        if indexed_documents is not None:
+            docs, index = zip(*indexed_documents)
+            self._documents = pd.Series(docs, index)
+        elif documents_series is not None:
+            self._documents = documents_series
+        else:
+            raise ValueError('set one of indexed_documents or documents_series')
+
+    @property
+    def index(self) -> pd.Index:
+        return self._documents.index
+
+    def __iter__(self) -> Iterator[Doc]:
+        for doc in self._documents:
+            yield doc
+
+    def __len__(self) -> int:
+        return len(self._documents)
+
+    def __repr__(self) -> str:
+        return repr(self._documents)
+
+    def __getitem__(self, index: Union[Sequence[int], int]) -> Union[Doc, Corpus]:
+        if isinstance(index, int):
+            result = self._documents[index]
+        else:
+            result = Corpus(self.nlp, documents_series=self._select(index))
+
+        return result
+
+    def _select(self, index: Sequence[int] = None) -> pd.Series:
+        document: pd.Series
+
+        if index:
+            documents = self._documents[index]
+        else:
+            documents = self._documents
+
+        return documents
+
+    def sentences_generator(self, index: Optional[Sequence[int]] = None) -> Iterator[str]:
+        for doc in self._select(index):
+            for sent in doc.sents:
+                yield sent.text
+
+    def sentences(self, index: Optional[Sequence[int]] = None) -> Sequence[str]:
+        return list(self.sentences_generator(index))
+
+    def texts_generator(self, index: Optional[Sequence[int]] = None) -> Iterator[str]:
+        for doc in self._select(index):
+            yield doc.text
+
+    def texts(self, index: Optional[Sequence[int]] = None) -> Sequence[str]:
+        return list(self.texts_generator(index))
+
+    def pos_tags_generator(self, index: Optional[Sequence[int]] = None) -> Iterator[str]:
+        for doc in self._select(index):
+            yield [token.tag_ for token in doc]
+
+    def pos_tags(self, index: Optional[Sequence[int]] = None) -> Sequence[str]:
+        return list(self.pos_tags_generator(index))
+
+    def count_sentences(self) -> int:
+        return np.sum(len(doc.sents) for doc in self._documents)
+
+    def count_words(self) -> int:
+        return np.sum(len(doc) for doc in self._documents)
+
+    def save(self, path: Union[str, Path]) -> None:
+        '''
+        Archive the serialized document in a zip file.
+        '''
+        with zipfile.ZipFile(path, 'w', compression=zipfile.ZIP_DEFLATED) as archive:
+            for i, doc in self._documents.iteritems():
+                data = doc.to_bytes(exclude=['tensor'])
+                archive.writestr(f'{i}.pickle', data)
+
+    @classmethod
+    def load(cls, nlp: Language, path: Union[str, Path]) -> Corpus:
+        index = []
+        docs = []
+        with zipfile.ZipFile(path, 'r', compression=zipfile.ZIP_DEFLATED) as archive:
+            for fname in archive.namelist():
+                i, _ = os.path.splitext(fname)
+                index.append(i)
+                with archive.open(fname) as f:
+                    doc = Doc(nlp.vocab).from_bytes(f.read())
+                    docs.append(doc)
+        indexed_documents = zip(docs, index)
+
+        return cls(nlp, indexed_documents=indexed_documents)
--- src/utils/nn.py
+++ src/utils/nn.py
@@ -1,11 +1,72 @@
-from typing import Union, Dict
+from typing import Union, Dict, Iterable
 from pathlib import Path
 
 from sklearn.base import BaseEstimator, TransformerMixin
-
 from gensim.models.keyedvectors import WordEmbeddingsKeyedVectors
-
+from gensim.models import FastText
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.callbacks import History
 import numpy as np
+
+from .corpus import Corpus
+
+
+class ModelWrapper:
+    '''
+    Wraps a keras model and expose a common interface.
+    '''
+
+    model: Union[Sequential, Model]
+    name: str
+
+    def __init__(self, tokenizers: dict, use_softmax: bool = True, class_weight: dict = None):
+        self.tokenizers = tokenizers
+        self._score_scaler = ScoreScaler()
+        self.use_softmax = use_softmax
+        self.class_weight = class_weight
+
+    def compile(self) -> Union[Sequential, Model]:
+        '''
+        Compiles the model
+        '''
+        self.model = self._compile()
+
+        return self.model
+
+    def _compile(self) -> Union[Sequential, Model]:
+        '''
+        Implement to create the underlying Keras model.
+        '''
+        raise NotImplementedError
+
+    def fit(self, x, y, *args, **kwargs) -> History:
+        '''
+        Calls the fit method of the keras model and returns the history.
+        '''
+
+        if self.use_softmax:
+            y = y - 1
+            class_weight = self.class_weight
+        else:
+            y = self._score_scaler.fit_transform(y)
+            class_weight = None
+
+        return self.model.fit(x, y, class_weight=class_weight, *args, **kwargs)
+
+    def predict(self, x) -> np.ndarray:
+        if self.use_softmax:
+            cat_pred = self.model.predict_classes(x)
+            y_pred = cat_pred + 1
+        else:
+            y_pred = predict_scores(x, self.model, self._score_scaler)
+
+        return y_pred
+
+    def text_to_padded_sequences(self, corpus: Corpus) -> Iterable:
+        '''
+        Extract the relevant information for the model from the corpus
+        '''
+        raise NotImplementedError
 
 
 def load_word_vectors(path: Union[str, Path]) -> dict:
@@ -48,6 +109,7 @@ class ScoreScaler(BaseEstimator, TransformerMixin):
 
     def inverse_transform(self, y):
         scores = np.round(y * self._max).astype(np.int).flatten()
+
         return np.clip(scores, self._min, self._max)
 
 
@@ -56,3 +118,17 @@ def predict_scores(X, model, scaler):
     scores = scaler.inverse_transform(y)
 
     return scores
+
+
+def train_fasttext(corpus_file: str, size: int, total_words: int, epochs: int, window=5) -> FastText:
+    '''
+    Train the Facebook FastText model on the sentences in the corpus file.
+    '''
+    vector_model = FastText(size=size, window=window, min_count=1, sg=1)
+    vector_model.build_vocab(corpus_file=corpus_file)
+    vector_model.train(corpus_file=corpus_file,
+                       total_examples=vector_model.corpus_count,
+                       total_words=total_words,
+                       epochs=epochs)
+
+    return vector_model
